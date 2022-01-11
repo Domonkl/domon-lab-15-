a25a55fd719cf6758f2e1b1877e722b17d96d7e2
Domonmez <domonsus@gmail.com>
2022-01-11 16:57:47 +0700

    ข้อ 1

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,8 +3,26 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+int i,j;
+    double z;
+    for (i = 1; i < N; i++)
+    { 
+        z = d[i]; 
+        j = i - 1; 
+
+        while (j >= 0 && d[j] < z)
+        { 
+            d[j + 1] = d[j]; 
+            j = j - 1; 
+        } 
+        d[j + 1] = z; 
+        cout << "Pass " << i << ":";
+        for (int x = 0; x < 10; x++) cout << d[x] << " ";
+        cout << "\n";
+    } 
 
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
